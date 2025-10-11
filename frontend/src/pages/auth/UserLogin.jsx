5445bf888aa0d75add7c977d46a54689efa84bdc
Harsh140710 <sutharharsh108@gmail.com>
2025-10-11 20:51:06 +0530

    feat: implement user login functionality with form handling and API integration

--- frontend/src/pages/auth/UserLogin.jsx
+++ frontend/src/pages/auth/UserLogin.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "../../index.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const UserLogin = () => {
+
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const response = await axios.post("http://localhost:3000/api/auth/user/login",{
+      email: email,
+      password: password,
+    }, {
+      withCredentials: true,
+    });
+
+    console.log(response.data);
+
+    navigate("/")
+    
+  }
+
   return (
     <div className="auth-container">
       <div className="auth-card">
         <h2 className="auth-title">User Login</h2>
-        <form className="auth-form">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <form className="auth-form" onSubmit={handleSubmit}>
+          <input type="email" id="email" placeholder="Email" />
+          <input type="password" id="password" placeholder="Password" />
           <button className="auth-btn">Sign In</button>
         </form>
         <p className="auth-link">
